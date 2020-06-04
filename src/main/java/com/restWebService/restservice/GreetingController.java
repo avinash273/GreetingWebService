b5a6067eb834b111e1477ab267ee72d483b32053
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-04 02:30:56 -0500

    Initial commit

---
+++ src/main/java/com/restWebService/restservice/GreetingController.java
@@ -0,0 +1,22 @@
+package com.restWebService.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * In Spring’s approach to building RESTful web services, HTTP requests are handled by a controller.
+ * These components are identified by the @RestController annotation
+ */
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
